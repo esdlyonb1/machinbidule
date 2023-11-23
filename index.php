0d378ed6ed5ad999444fc96bc6de4ce06f6a0edb
esdb1 <pierre.lacognata@mail-esd.com>
2023-11-23 10:08:23 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$truc = "coucou";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>mon site web</title>
+</head>
+<body>
+<h3>Coucou</h3>
+
+<h2><?= $truc ?></h2>
+
+</body>
+</html>
